53b06f780fa424f43593173d08500d66f7ef33c4
Stefanie-A <albertstephanie630@gmail.com>
2024-08-05 12:26:07 +0100

    feat: database details

--- task.py
+++ task.py
@@ -1,16 +1,21 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
 
 conn = psycopg2.connect(
-    host = "localhost",
-    user = "",
-    database = "",
-    password = "",
-    port = 5432 
+    host = os.getenv("host"),
+    user = os.getenv("username"),
+    database = os.getenv("database"),
+    password = os.getenv("password"),
+    port = os.getenv("port") 
 )
 
 cur = conn.cursor()
 cur.execute("""CREATE TABLE task
-(id SERIAL PRIMARY KEY,
+(id BIGSERIAL PRIMARY KEY,
 name VARCHAR(50) UNIQUE NOT NULL, 
 isACtive BOOLEAN DEFAULT NOT TRUE ); 
 """)
